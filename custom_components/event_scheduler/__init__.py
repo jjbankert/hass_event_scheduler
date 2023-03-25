aa10d9d1c1a8e70bcc989be360676e81cf33b37b
Jelle Jan Bankert <jjbankert@gmail.com>
2023-03-25 18:31:10 +0100

    streamlined variable names, added services fields

--- custom_components/event_scheduler/__init__.py
+++ custom_components/event_scheduler/__init__.py
@@ -8,29 +8,32 @@ DOMAIN = "event_scheduler"
 
 
 def setup(hass: HomeAssistant, config: Config):
-    def upsert_scheduled_event(call: ServiceCall):
+    def upsert_event(call: ServiceCall):
         params: dict[str, Any] = dict(call.data)
         event_id = f"{DOMAIN}.{params.pop('id')}"
-        hass.states.set(event_id, params.pop("trigger_time"), params)
+        hass.states.set(event_id, params.pop("time"), params)
 
-    def remove_scheduled_event(call: ServiceCall):
+    def remove_event(call: ServiceCall):
         event_id = f"{DOMAIN}.{call.data['id']}"
         hass.states.remove(event_id)
 
     def fire_events(call: ServiceCall):
-        # store the events before we start removing them
+        # get all scheduled events
         events = hass.states.all(DOMAIN)
 
         for event in events:
+            # find the events that should be fired now
             if datetime.fromisoformat(event.state) <= datetime.now(timezone.utc):
-                # remove from scheduled events
+                # Remove from scheduled events first. This ensures that a triggered event
+                # can schedule a new event with the same id without fear of race conditions.
+                # It can be a potential issue if the hass.bus.fire fails for some reason.
                 hass.states.remove(event.entity_id)
 
                 # fire event
-                hass.bus.fire(event.attributes["channel"], event.attributes["data"])
+                hass.bus.fire(event.attributes["type"], event.attributes["data"])
 
-    hass.services.register(DOMAIN, "upsert_scheduled_event", upsert_scheduled_event)
-    hass.services.register(DOMAIN, "remove_scheduled_event", remove_scheduled_event)
+    hass.services.register(DOMAIN, "upsert_event", upsert_event)
+    hass.services.register(DOMAIN, "remove_event", remove_event)
     hass.services.register(DOMAIN, "fire_events", fire_events)
 
     # Return boolean to indicate that initialization was successful.
